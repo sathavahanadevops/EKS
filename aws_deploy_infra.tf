441f8e2a9ce5724d0ca3d3d36450d0d26a7bcb8a
sathavahana <48242515+sathavahanadevops@users.noreply.github.com>
2024-01-06 08:43:04 +0530

    Update aws_deploy_infra.tf

--- aws_deploy_infra.tf
+++ aws_deploy_infra.tf
@@ -12,6 +12,8 @@ resource "aws_instance" "manage" {
   associate_public_ip_address = true
   user_data                   = <<-EOF
     #!/bin/bash
+    sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     sudo yum upgrade -y
     sudo dnf install java-17-amazon-corretto -y
