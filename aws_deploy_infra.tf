69a4e7fc90fac825239e9353b649bc381d869975
sathavahana <48242515+sathavahanadevops@users.noreply.github.com>
2024-01-06 07:53:30 +0530

    Create aws_deploy_infra.tf

---
+++ aws_deploy_infra.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "manage" {
+  ami                         = "ami-079db87dc4c10ac91"
+  availability_zone           = "us-east-1"
+  instance_type               = "t2.medium"
+  key_name                    = "demo"
+  subnet_id                   = "subnet-060372cf8cd26124f"
+  vpc_security_group_ids      = ["sg-08faf759e7e05c47f"]
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+    #!/bin/bash
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    sudo yum upgrade -y
+    sudo dnf install java-17-amazon-corretto -y
+    sudo yum install jenkins -y
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+    sudo yum install git -y
+    sudo yum install docker -y
+    sudo usermod -a -G docker ec2-user
+    sudo yum install python3-pip
+    sudo systemctl enable docker.service
+    sudo systemctl start docker.service
+    EOF
+  tags = {
+    Name = "Dmanage"
+  }
+}
